5cee4a8944e1b3dcc49ceb419fdc415acb3d11b9
VZout <viktor.zoutman@embark-studios.com>
2020-10-22 13:00:43 +0200

    minor fix

--- examples/wgpu-example-shader/src/lib.rs
+++ examples/wgpu-example-shader/src/lib.rs
@@ -1,20 +1,23 @@
 //! Ported to Rust from https://github.com/Tw1ddle/Sky-Shader/blob/master/src/shaders/glsl/sky.fragment
 
 #![no_std]
+#![feature(lang_items)]
 #![feature(register_attr)]
 #![register_attr(spirv)]
 
 use core::panic::PanicInfo;
 use spirv_std::{Input, Output, Vec4};
 
+#[allow(unused_attributes)]
 #[spirv(entry = "fragment")]
 pub fn main_fs(mut output: Output<Vec4>) {
     output.store(Vec4::new(1.0, 0.0, 0.0, 1.0))
 }
 
+#[allow(unused_attributes)]
 #[spirv(entry = "vertex")]
 pub fn main_vs(
-    #[spirv(builtin = "vertex_index")] mut vert_id: Input<i32>,
+    #[spirv(builtin = "vertex_index")] vert_id: Input<i32>,
     #[spirv(builtin = "position")] mut out_pos: Output<Vec4>,
 ) {
     let vert_id = vert_id.load();
@@ -26,7 +29,12 @@ pub fn main_vs(
     ));
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_: &PanicInfo) -> ! {
     loop {}
 }
+
+#[cfg(not(test))]
+#[lang = "eh_personality"]
+extern "C" fn rust_eh_personality() {}
